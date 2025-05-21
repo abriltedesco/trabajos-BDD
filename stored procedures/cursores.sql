81f953a8160efba2c2ab6b3681a63ef7ea6802c2
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-05-21 19:27:50 -0300

    Update cursores.sql

--- stored procedures/cursores.sql
+++ stored procedures/cursores.sql
@@ -3,7 +3,7 @@ ingresos de esta semana. */
 delimiter //
 create procedure actualizarStock()
 begin
-	declare hayFilas boolean default 1;
+    declare hayFilas boolean default 1;
     declare prodObtenido int default 0;
     declare cantObtenida int default 0;
     declare prodCursor CURSOR FOR SELECT Producto_codProducto, sum(cantidad) as cant
@@ -14,26 +14,24 @@ begin
     
     open prodCursor;
     ploop:loop
-		fetch prodCursor into prodObtenido, cantObtenida;
-			if hayFilas = 0 then
-				leave ploop;
-            end if;
-            UPDATE producto SET stock = stock + cantObtenida WHERE codProducto = prodObtenido;
+	fetch prodCursor into prodObtenido, cantObtenida;
+		if hayFilas = 0 then
+			leave ploop;
+                end if;
+        UPDATE producto SET stock = stock + cantObtenida WHERE codProducto = prodObtenido;
 	end loop;
     close prodCursor;
 end //
 delimiter ;
 
 call actualizarStock();
+
 /*  2. Crear un Stored Procedure que reduzca el precio de los productos en un 10% si no se
 vendieron más de 100 unidades en la semana. */
-
-DROP PROCEDURE reducirPrecio;
-
 delimiter //
 create procedure reducirPrecio()
 begin
-	declare hayFilas boolean default 1;
+    declare hayFilas boolean default 1;
     declare prodObtenido int default 0;
     declare cantObtenida int default 0;
     declare prodCursor CURSOR FOR SELECT Producto_codProducto, sum(cantidad) as c
@@ -44,15 +42,15 @@ begin
     
     open prodCursor;
     ploop:loop
-		fetch prodCursor into prodObtenido, cantObtenida;
-			if hayFilas = 0 then
-				leave ploop;
-            end if;
+	fetch prodCursor into prodObtenido, cantObtenida;
+		if hayFilas = 0 then
+			leave ploop;
+        	end if;
             
-            if cantObtenida < 100 then
+        if cantObtenida < 100 then
              UPDATE producto SET precio = precio - (precio * 0.1) WHERE codProducto = prodObtenido;
-            end if;
-	end loop;
+        end if;
+    end loop;
     close prodCursor;
 end //
 delimiter ;
@@ -65,7 +63,7 @@ que el mayor precio al que lo proveen los proveedores. */
 delimiter //
 create procedure actualizarPrecio()
 begin
-	declare hayFilas boolean default 1;
+    declare hayFilas boolean default 1;
     declare prodObtenido int default 0;
     declare maxObtenido int default 0;
     declare prodCursor CURSOR FOR SELECT Producto_codProducto, max(precio) as maximo
@@ -74,13 +72,14 @@ begin
     
     open prodCursor;
     ploop:loop
-		fetch prodCursor into prodObtenido, maxObtenido;
-			if hayFilas = 0 then
-				leave ploop;
-            end if;
-			UPDATE producto SET precio = precio + maxObtenido + (maxObtenido * 0.1) 
-            WHERE codProducto = prodObtenido;
-	end loop;
+	fetch prodCursor into prodObtenido, maxObtenido;
+		if hayFilas = 0 then
+			leave ploop;
+                end if;
+			
+	UPDATE producto SET precio = precio + maxObtenido + (maxObtenido * 0.1) 
+        WHERE codProducto = prodObtenido;
+    end loop;
     close prodCursor;
 end //
 delimiter ;
@@ -96,7 +95,7 @@ ingresos inclusive, “Plata” de 50 a 100 ingresos inclusive y “Oro” más 
 delimiter //
 create procedure crearNivel()
 begin
-	declare hayFilas boolean default 1;
+    declare hayFilas boolean default 1;
     declare provObtenido int default 0;
     declare cantIngresos int default 0;
     declare provCursor CURSOR FOR SELECT Proveedor_idProveedor, count(*) as cant
@@ -106,21 +105,21 @@ begin
     
     open provCursor;
     ploop:loop
-		fetch provCursor into provObtenido, cantIngresos;
-			if hayFilas = 0 then
-				leave ploop;
-            end if;
+	fetch provCursor into provObtenido, cantIngresos;
+		if hayFilas = 0 then
+			leave ploop;
+                end if;
             
-			if cantIngresos <= 50 then
-				UPDATE proveedor SET nivel = "Bronce" WHERE idProveedor = provObtenido;
-            else if (cantIngresos > 50 AND cantIngresos < 100) then
-				UPDATE proveedor SET nivel = "Plata" WHERE idProveedor = provObtenido;
-			else
-				UPDATE proveedor SET nivel = "Oro" WHERE idProveedor = provObtenido;
-            end if;
-            end if;
+	if cantIngresos <= 50 then
+		UPDATE proveedor SET nivel = "Bronce" WHERE idProveedor = provObtenido;
+        else if (cantIngresos > 50 AND cantIngresos < 100) then
+		UPDATE proveedor SET nivel = "Plata" WHERE idProveedor = provObtenido;
+	else
+		UPDATE proveedor SET nivel = "Oro" WHERE idProveedor = provObtenido;
+        end if;
+        end if;
             
-	end loop;
+    end loop;
     close provCursor;
 end //
 delimiter ;
@@ -133,7 +132,7 @@ pedidos pendientes de pago, al precio actual del producto */
 delimiter //
 create procedure actualizarPrecioUnit()
 begin
-	declare hayFilas boolean default 1;
+    declare hayFilas boolean default 1;
     declare prodObtenido int default 0;
     declare precioObtenido int default 0;
     declare idPedObtenido int default 0;
@@ -146,14 +145,14 @@ begin
     
     open prodCursor;
     ploop:loop
-		fetch prodCursor into prodObtenido, idPedObtenido, precioObtenido;
-			if hayFilas = 0 then
-				leave ploop;
-            end if;
+	   fetch prodCursor into prodObtenido, idPedObtenido, precioObtenido;
+		if hayFilas = 0 then
+			leave ploop;
+                end if;
                         
             UPDATE pedido_producto SET precioUnitario = precioUnitario + precioObtenido
             WHERE Producto_codProducto = prodObtenido AND Pedido_idPedido = idPedObtenido;
-	end loop;
+    end loop;
     close prodCursor;
 end //
 delimiter ;
