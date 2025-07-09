3585218b93ab59c088d2b6408a6d741c18550a3c
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2025-07-08 22:00:08 -0300

    ej 5 completo

--- triggers/modelo-stock.sql
+++ triggers/modelo-stock.sql
@@ -2,14 +2,14 @@
 IngresoStock_Producto restando la cant de esta tabla la cant informada en Pedido_Producto. */
 
 delimiter //
-create trigger after_insert_pedProducto before insert on pedido_producto for each row
+create trigger after_insert_pedProducto after insert on pedido_producto for each row
 begin
 	UPDATE ingresostock_producto SET cantidad = cantidad - new.cantidad 
     WHERE Producto_codProducto = new.Producto_codProducto;
 end //
 delimiter ;
 
-INSERT INTO pedido_producto VALUE (15, 10, 150.00, 4, 12);
+INSERT INTO pedido_producto VALUE (16, 5, 170.00, 1, 12);
 
 /* 2) trigger que antes de borrar en la tabla IngresoStock borre todas las filas de la tabla
 IngresoStock_Producto. */
@@ -20,7 +20,7 @@ begin
 end //
 delimiter ;
 
-DELETE FROM ingresostock where idIngreso = 5;
+DELETE FROM ingresostock where idIngreso = 34;
 
 /* 3) Imaginando que agregamos una columna categoría en clientes, hacer un trigger que, cada vez
 que se agrega un pedido, se calcule el monto total gastado por ese cliente en los últimos dos años y
@@ -29,33 +29,32 @@ actualice la categoría del cliente. Las categorías son “bronce” hasta $50.
 
 drop procedure categoriaCliente;
 ALTER TABLE cliente add COLUMN categoria VARCHAR(15) NULL ;
-delimiter //
 
+delimiter //
 create procedure categoriaCliente(in idCliente int)
 begin
 	declare hayFilas boolean default 1;
     declare cantGastoObtenido int default 0;
-    declare clienteObtenido int default 0;
     
     declare gastoCursor CURSOR FOR 
-		SELECT sum(cantidad * precioUnitario), Cliente_codCliente FROM pedido 
+		SELECT sum(cantidad * precioUnitario) FROM pedido 
         JOIN pedido_producto ON Pedido_idPedido = idPedido
 		GROUP BY Cliente_codCliente HAVING Cliente_codCliente = idCliente ;
     declare continue handler for not found set hayFilas = 0;
     
     open gastoCursor;
     gloop:loop
-		fetch gastoCursor into cantGastoObtenido, clienteObtenido;
+		fetch gastoCursor into cantGastoObtenido;
 			if hayFilas = 0 then
 				leave gloop;
             end if;
             
 			if cantGastoObtenido <= 5000 then
-				UPDATE cliente SET categoria = "Bronce" WHERE codCliente = clienteObtenido;
+				UPDATE cliente SET categoria = "Bronce" WHERE codCliente = idCliente;
             else if (cantGastoObtenido > 5000 AND cantGastoObtenido  < 10000) then
-				UPDATE cliente SET categoria = "Plata" WHERE codCliente = clienteObtenido;
+				UPDATE cliente SET categoria = "Plata" WHERE codCliente = idCliente;
 			else
-				UPDATE cliente SET categoria = "Oro" WHERE codCliente = clienteObtenido;
+				UPDATE cliente SET categoria = "Oro" WHERE codCliente = idCliente;
             end if;
             end if;
             
@@ -72,7 +71,8 @@ begin
 	call categoriaCliente(new.Cliente_codCliente);
 end //
 delimiter ;
-INSERT INTO pedido VALUE (29, current_date(), 1, 9);
+
+INSERT INTO pedido VALUE (36, current_date(), 3, "3");
 
 /* 4) Realizar un trigger que después de insertar una fila en la tabla de IngresoStock_Producto incremente la
 columna stock de la tabla de productos con la cantidad ingresada de la tabla IngresoStock_Producto. */
@@ -90,3 +90,13 @@ trigger que me permita llevar a cabo la acción de borrado en la tabla de pedido
 cuáles serían otras alternativas de solución que se les ocurra, como por ejemplo utilizando una función o
 definiendo cambio de restricciones, alguna otra característica que le parezca contemplar. */
 
+delimiter //
+create trigger before_delete_pedidos before delete on pedido for each row
+begin
+	DELETE FROM pedido_producto where Pedido_idPedido = (old.idPedido);
+end //
+delimiter ;
+
+DELETE FROM pedido WHERE idPedido = 8;
+ 
+-- sin triggers seria con delete cascade o una funcion o procedure q pongas id y te borre directamente en todas las tablas con ese id
